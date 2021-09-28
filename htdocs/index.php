e5ad05bf9ad822db6c34d1199c6e1bf18c5e008c
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-09-29 00:38:03 +0200

    htdocs: index.php added navbar and routing.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,17 @@
 <?php session_start(); ?>
-<?php require_once 'protected/config.php'; ?>
-<?php
-
+<?php require_once 'protected/config.php';
 ?>
+<?php error_reporting (E_ALL ^ E_NOTICE); ?>
+<!DOCTYPE html>
+
 <html>
     <head>
-        <link rel="stylesheet" href="public/style.css">
+        <meta charset="utf-8">
+	    <title>Órarend</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
-
+    <body>
+        <nav><?php require_once PROTECTED_DIR.'navbar.php'; ?></nav>
+        <content><?php require_once PROTECTED_DIR.'routing.php'; ?></content>
+    </body>
 </html>
