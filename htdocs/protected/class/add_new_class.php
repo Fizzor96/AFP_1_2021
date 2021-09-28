52e65eb7ae9d8d0542cb443f402f7fa41074c579
ricsirobi <acs.robert2000@gmail.com>
2021-09-28 13:15:08 +0200

    htdocs: updated add_new_class.php backend

--- htdocs/protected/class/add_new_class.php
+++ htdocs/protected/class/add_new_class.php
@@ -1,35 +1,43 @@
 <?php
 require "../database.php";
+if(isset($_POST["in"]) && $_POST["name"]!=null && $_POST["place"] != null && $_POST["time"] != null && $_POST["prof"] != null)
+{
+    if($_POST["name"]!="" && $_POST["place"] != "" && $_POST["time"] != "" && $_POST["prof"] != "")
+    {
+            //toltsd fel az adatokat
+    }
+
+}
 ?>
 <html>
 <body>
     <table>
-    <form>
+    <form method='post' action=''>
     <tr>
         <td>
-            <input type = "text" placeholder="Tárgy neve">
+            <input type = "text" placeholder="Tárgy neve" name="name">
         </td>
     </tr>
     <tr>
         <td>
-            <input type = "text" placeholder="Óra helye">
+            <input type = "text" placeholder="Óra helye" name="place">
         </td>
     </tr>
     <tr>
         <td>
-            <input type = "text" placeholder="Időpont">
+            <input type = "text" placeholder="Időpont" name="time">
         </td>
     </tr>
     <tr>
         <td>
-            <input type = "text" placeholder="Tanár neve">
+            <input type = "text" placeholder="Tanár neve" name="prof">
         </td>
     </tr>
-        <tr>
-            <td>
-                <input type = "submit" value = beküldés> 
-            </td>
-        </tr>
+    <tr>
+        <td>
+            <input type = "submit" value = "Beküldés" name="in"> 
+        </td>
+    </tr>
  
 
     </form>
