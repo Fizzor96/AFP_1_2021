dd71c8cb723f4252d5370b91e2700e57538baaee
ricsirobi <acs.robert2000@gmail.com>
2021-09-28 13:02:30 +0200

    htdocs: updated add_new_class.php: added inputs with button

--- htdocs/protected/class/add_new_class.php
+++ htdocs/protected/class/add_new_class.php
@@ -1,5 +1,40 @@
 <?php
 require "../database/database.php";
-
-
 ?>
+<html>
+<body>
+    <table>
+    <form>
+    <tr>
+        <td>
+            <input type = "text" placeholder="Tárgy neve">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type = "text" placeholder="Óra helye">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type = "text" placeholder="Időpont">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type = "text" placeholder="Tanár neve">
+        </td>
+    </tr>
+
+
+        <tr>
+            <td>
+                <input type = "submit" value = beküldés> 
+            </td>
+        </tr>
+ 
+
+    </form>
+    </table>
+</body>
+</html>
