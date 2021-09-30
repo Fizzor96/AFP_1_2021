b1212e66d588942a116bd4856a4c222ea4b0bf71
ricsirobi <acs.robert2000@gmail.com>
2021-09-30 22:53:31 +0200

    htdocs: formatted php files

--- htdocs/protected/class/add_new_class.php
+++ htdocs/protected/class/add_new_class.php
@@ -2,63 +2,63 @@
 
 require_once DATABASE_CONTROLLER;
 
-if(isset($_POST["in"]) && $_POST["name"]!=null && $_POST["place"] != null && $_POST["time"] != null && $_POST["prof"] != null)
-{
-    if($_POST["name"]!="" && $_POST["place"] != "" && $_POST["time"] != "" && $_POST["prof"] != "")
-    {
-            $name = $_POST["name"];
-            $place = $_POST["place"];
-            $time = $_POST["time"];
-            $prof = $_POST["prof"];
+if (isset($_POST["in"]) && $_POST["name"] != null && $_POST["place"] != null && $_POST["time"] != null && $_POST["prof"] != null) {
+    if ($_POST["name"] != "" && $_POST["place"] != "" && $_POST["time"] != "" && $_POST["prof"] != "") {
+        $name = $_POST["name"];
+        $place = $_POST["place"];
+        $time = $_POST["time"];
+        $prof = $_POST["prof"];
 
-            //az adatbázis tábla nevét majd át kell írni a valódira, meg lehet kell még ezen az sql-n alakítani mivel a nap is kellene
-            //de valahogy így fog kinézni
+        //az adatbázis tábla nevét majd át kell írni a valódira, meg lehet kell még ezen az sql-n alakítani mivel a nap is kellene
+        //de valahogy így fog kinézni
 
-            $sql = "INSERT INTO targy (nev, hely, ora, tanar) VALUES ('" . $name . "', '". $place ."', '" . $time . "', '" . $prof . "')";
-            
-            executeQuery($sql);
+        $sql = "INSERT INTO targy (nev, hely, ora, tanar) VALUES ('" . $name . "', '" . $place . "', '" . $time . "', '" . $prof . "')";
+
+        executeQuery($sql);
     }
-
 }
 ?>
 <html>
-<HEAD>
-        <link rel="stylesheet" href="<?php echo PUBLIC_DIR."register.css";?>">
-        <link rel="stylesheet" href="../../public/register.css">
-    </HEAD>
-<body>
-<div class = "registerDiv">
-    <table class = "registerForm">
-    <form method='post' action=''>
-    <tr>
-        <td>
-            <input type = "text" placeholder="Tárgy neve" name="name">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type = "text" placeholder="Óra helye" name="place">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type = "text" placeholder="Időpont" name="time">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type = "text" placeholder="Tanár neve" name="prof">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type = "submit" value = "Beküldés" name="in"> 
-        </td>
-    </tr>
- 
 
-    </form>
-    </table>
-</div>
+<HEAD>
+    <link rel="stylesheet" href="<?php echo PUBLIC_DIR . "register.css"; ?>">
+    <link rel="stylesheet" href="../../public/register.css">
+</HEAD>
+
+<body>
+    <div class="registerDiv">
+        <table class="registerForm">
+            <form method='post' action=''>
+                <tr>
+                    <td>
+                        <input type="text" placeholder="Tárgy neve" name="name">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" placeholder="Óra helye" name="place">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" placeholder="Időpont" name="time">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" placeholder="Tanár neve" name="prof">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Beküldés" name="in">
+                    </td>
+                </tr>
+
+
+            </form>
+        </table>
+    </div>
 </body>
+
 </html>
--- htdocs/protected/user/register.php
+++ htdocs/protected/user/register.php
@@ -1,45 +1,47 @@
-
 <HTMl>
-    <HEAD>
-        <link rel="stylesheet" href="<?php echo PUBLIC_DIR."register.css";?>">
-        <link rel="stylesheet" href="../../public/register.css">
-    </HEAD>
-    <BODY>
-        <div class = "registerDiv">
-            <form method = "POST">
-                <table class = "registerForm">
-                    <tr>
-                        <td>
-                            <!--<p>Teljes név</p>-->
-                            <input type = "text" id = "register_fullname" placeholder = "Full name">
-                        </td>
-                    <tr>
-                    <tr>
-                        <td>
-                            <!--<p>Felhasználónév</p>-->
-                            <input type = "text" id = "reg_username" placeholder = "Username">
-                        </td>
-                    <tr>
-                    <tr>
-                        <td>
-                            <!--<p>Jelszó</p>-->
-                            <input type = "password" id = "reg_password" placeholder = "Password">
-                        </td>
-                    <tr>
-                    <tr>
-                        <td>
-                            <!--<p>Email</p>-->
-                            <input type = "email" id = "reg_email" placeholder = "Email">
-                        </td>
-                    <tr>
-                    <tr class = "loginText">
-                        <td colspan = 2>Már van felhasználód? <a href = "index.php?P=login"><span>Kattints ide!</span><a></td>
-                    <tr>
-                    <tr>
-                        <td colspan = 2><button>Register</button></td>
-                    <tr>
-                </table>
-            </form>
-        </div>
-    </BODY>
+
+<HEAD>
+    <link rel="stylesheet" href="<?php echo PUBLIC_DIR . "register.css"; ?>">
+    <link rel="stylesheet" href="../../public/register.css">
+</HEAD>
+
+<BODY>
+    <div class="registerDiv">
+        <form method="POST">
+            <table class="registerForm">
+                <tr>
+                    <td>
+                        <!--<p>Teljes név</p>-->
+                        <input type="text" id="register_fullname" placeholder="Full name">
+                    </td>
+                <tr>
+                <tr>
+                    <td>
+                        <!--<p>Felhasználónév</p>-->
+                        <input type="text" id="reg_username" placeholder="Username">
+                    </td>
+                <tr>
+                <tr>
+                    <td>
+                        <!--<p>Jelszó</p>-->
+                        <input type="password" id="reg_password" placeholder="Password">
+                    </td>
+                <tr>
+                <tr>
+                    <td>
+                        <!--<p>Email</p>-->
+                        <input type="email" id="reg_email" placeholder="Email">
+                    </td>
+                <tr>
+                <tr class="loginText">
+                    <td colspan=2>Már van felhasználód? <a href="index.php?P=login"><span>Kattints ide!</span><a></td>
+                <tr>
+                <tr>
+                    <td colspan=2><button>Register</button></td>
+                <tr>
+            </table>
+        </form>
+    </div>
+</BODY>
+
 </HTML>
