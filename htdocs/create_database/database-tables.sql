77b6c2e6a8616efaac397e52493e2ed31cfb0f5c
ricsirobi <acs.robert2000@gmail.com>
2021-09-30 22:44:20 +0200

    htdocs: database updated/fixed

--- htdocs/create_database/database-tables.sql
+++ htdocs/create_database/database-tables.sql
@@ -1,26 +1,27 @@
-DROP TABLE IF EXISTS `felhasznalok`;
-DROP TABLE IF EXISTS `orarend`;
-DROP TABLE IF EXISTS `ora`;
+DROP TABLE IF EXISTS felhasznalok;
+DROP TABLE IF EXISTS felhasznalok_ora;
+DROP TABLE IF EXISTS ora;
 
 
-CREATE TABLE `felhasznalok` (
-`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-`felhasznalonev` VARCHAR(255) NOT NULL,
-`jelszo` VARCHAR(255) NOT NULL,
-`email` VARCHAR(255) NOT NULL,
-`jog` INT(2) NOT NULL);
+CREATE TABLE felhasznalok (
+id INT NOT NULL PRIMARY KEY,
+felhasznalonev VARCHAR(255) NOT NULL,
+jelszo VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+jog BOOLean NOT NULL);
 
-CREATE TABLE `orarend` (
-`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-`felhasznaloid` INT NOT NULL,
-`oraid` INT NOT NULL);
+CREATE TABLE felhasznalok_ora (
+id INT NOT NULL PRIMARY KEY,
+felhasznaloid INT NOT NULL,
+oraid INT NOT NULL);
 
-CREATE TABLE `ora` (
-`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-`nev` VARCHAR(100) NOT NULL,
-`ido` TIME NOT NULL,
-`nap` VARCHAR(55) NOT NULL,
-`tanarnev` VARCHAR(255) NOT NULL);
+CREATE TABLE ora (
+id INT NOT NULL PRIMARY KEY,
+nev VARCHAR(100) NOT NULL,
+ido ENUM ("08:00-10:00","10:00-12:00","12:00-14:00","14:00-16:00","16:00-18:00","18:00-20:00") NOT NULL,
+nap ENUM ("1","2","3","4","5") NOT NULL,
+hely VARCHAR(255) NOT NULL,
+tanarnev VARCHAR(255) NOT NULL);
 
-ALTER TABLE `orarend` ADD CONSTRAINT `orarend_felhasznaloid_felhasznalok_id` FOREIGN KEY (`felhasznaloid`) REFERENCES `felhasznalok`(`id`);
-ALTER TABLE `orarend` ADD CONSTRAINT `orarend_oraid_ora_id` FOREIGN KEY (`oraid`) REFERENCES `ora`(`id`);
+ALTER TABLE felhasznalok_ora ADD CONSTRAINT felhasznalok_ora_felhasznaloid_felhasznalok_id FOREIGN KEY (felhasznaloid) REFERENCES felhasznalok(id);
+ALTER TABLE felhasznalok_ora ADD CONSTRAINT felhasznalok_ora_oraid_ora_id FOREIGN KEY (oraid) REFERENCES ora(id);
