233ed81db4d28a31e0ab17d906a5e9709818993e
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-09-30 22:20:58 +0200

    htdocs: index.php updated

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,6 +9,7 @@
         <meta charset="utf-8">
 	    <title>Órarend</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo PUBLIC_DIR."style.css";?>">
     </head>
     <body>
         <nav><?php require_once PROTECTED_DIR.'navbar.php'; ?></nav>
