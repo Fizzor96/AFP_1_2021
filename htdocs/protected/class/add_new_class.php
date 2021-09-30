29324ca78b91088ba8e7b685dd49b9799b5ed605
Vereb Barna <intelbarna97@gmail.com>
2021-09-30 17:13:03 +0200

    feat: you can add new classes to database

--- htdocs/protected/class/add_new_class.php
+++ htdocs/protected/class/add_new_class.php
@@ -1,9 +1,22 @@
 <?php
+
+require_once DATABASE_CONTROLLER;
+
 if(isset($_POST["in"]) && $_POST["name"]!=null && $_POST["place"] != null && $_POST["time"] != null && $_POST["prof"] != null)
 {
     if($_POST["name"]!="" && $_POST["place"] != "" && $_POST["time"] != "" && $_POST["prof"] != "")
     {
-            //toltsd fel az adatokat
+            $name = $_POST["name"];
+            $place = $_POST["place"];
+            $time = $_POST["time"];
+            $prof = $_POST["prof"];
+
+            //az adatbázis tábla nevét majd át kell írni a valódira, meg lehet kell még ezen az sql-n alakítani mivel a nap is kellene
+            //de valahogy így fog kinézni
+
+            $sql = "INSERT INTO targy (nev, hely, ora, tanar) VALUES ('" . $name . "', '". $place ."', '" . $time . "', '" . $prof . "')";
+            
+            executeQuery($sql);
     }
 
 }
