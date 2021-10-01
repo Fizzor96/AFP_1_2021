86081faf7c5d32a90fef57b99b45cc8e8de0dcda
Vereb Barna <intelbarna97@gmail.com>
2021-10-01 15:57:05 +0200

    feat: moking aka mókolás

---
+++ htdocs/protected/class/del_class.php
@@ -0,0 +1,14 @@
+<br/>
+
+<h2>Felhasználók listája</2>
+
+<?php 
+
+$query = "SELECT o.id, o.nev FROM ora o, felhasznalok_ora f WHERE " . $_SESSION["uid"] . " = f.felhasznaloid AND f.oraid=o.id";
+
+$result = classList($query);
+
+?>
+
+<?php if($result === NULL || empty($result)): ?>
+    
