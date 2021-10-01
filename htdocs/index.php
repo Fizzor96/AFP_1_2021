fe89887f71d503dfadabbcd5e3b5cca602ac9841
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-10-01 21:35:59 +0200

    htdocs: index included alert script

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,6 +11,20 @@ require_once DATABASE_CONTROLLER;
 	    <title>Órarend</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="<?php echo PUBLIC_DIR."style.css";?>">
+        <script>
+            function alertText(id,msg,alertType)
+            {
+                if(alertType == "success")
+                {
+                document.getElementById(id).style.color = "#66CD00";
+                }
+                else
+                {
+                    document.getElementById(id).style.color = "crimson";
+                }
+                document.getElementById (id). innerHTML = msg;
+            }
+        </script>
     </head>
     <body>
         <nav><?php require_once PROTECTED_DIR.'navbar.php'; ?></nav>
