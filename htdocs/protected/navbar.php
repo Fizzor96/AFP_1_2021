f92d431b658ff08be5d4aa58e2b0a8cee372f8e4
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-10-01 22:16:36 +0200

    fix: navbar added if-else criterion

--- htdocs/protected/navbar.php
+++ htdocs/protected/navbar.php
@@ -2,13 +2,14 @@
 
     
     <div class="menuBar">
-      <h1 class="logo"><?php  echo ($_SESSION["uid"]? 'Bejelentkezve' : 'Üdvözöljük'); ?></h1>
+      <h1 class="logo"><?php  echo ($_SESSION["uid"]? 'Üdvözöljük, '.$_SESSION["felhasznalonev"].'' : 'Jelenetkezzen be!'); ?></h1>
       <ul>
+        <?php if($_SESSION["uid"] == NULL): ?>
         <li><a href="index.php?P=login">Bejelentkezés</a></li>
         <li><a href="index.php?P=register">Regisztráció</a></li>
         <li><a href="index.php?P=betekinto">Betekintő</a></li>
+        <?php else: ?>
         <li><a href="#">Órarend     <i class="fa fa-caret-down"></i></a>
-
             <div class="dropdownMenu">
                 <ul>
                   <li><a href="index.php?P=addNewClass">Új tárgy</a></li>
@@ -18,7 +19,7 @@
               </div>
         </li>
         <li><a href="index.php?P=logout">Kijelentkezés</a></li>
-        
+        <?php endif; ?>
       </ul>
     </div>
         
