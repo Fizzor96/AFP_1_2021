c4a78111d5fa33e4c37bb4c65612bac44275d8cc
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-10-01 22:15:17 +0200

    fix: routing modified and added new cases

--- htdocs/protected/routing.php
+++ htdocs/protected/routing.php
@@ -1,17 +1,24 @@
 <?php 
 if(!array_key_exists('P', $_GET) || empty($_GET['P']))
-	$_GET['P'] = 'login';
+	$_GET['P'] = 'home';
 
 
 	switch ($_GET['P']) 
     {
-		case 'login': require_once PROTECTED_DIR.'user/login.php'; break;
-        case 'register': require_once PROTECTED_DIR.'user/register.php'; break;
+		case 'login': 
+			!isUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php');
+			 break;
+        case 'register': 
+			!isUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php');
+			break;
+        case 'addNewClass': 
+			isUserLoggedIn() ? require_once PROTECTED_DIR.'class/add_new_class.php' : header('Location: index.php');
+			break;
+		case 'deleteFromTimeTable': 
+			isUserLoggedIn() ? require_once PROTECTED_DIR.'class/del_class.php' : header('Location: index.php');
+			break;
 
-        case 'addNewClass': require_once PROTECTED_DIR.'class/add_new_class.php'; break;
-		case 'deleteFromTimeTable': require_once PROTECTED_DIR.'class/del_class.php'; break;
-
-        case 'home': require_once PROTECTED_DIR.'user/login.php'; break;
+		case 'logout': userLogout(); break;
 		default: require_once PROTECTED_DIR.'404.php'; break;
 	}
 
