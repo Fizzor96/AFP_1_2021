d0fbf9ed915e63d074d672cdffb25e07c490a900
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-10-02 20:41:31 +0200

    fix: navbar add active nav links

--- htdocs/protected/navbar.php
+++ htdocs/protected/navbar.php
@@ -23,27 +23,27 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-        <a class="nav-link" href="index.php?P=home">Kezdőlap</a>
+        <a <?php if($_GET['P'] == "home"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=home">Kezdőlap</a>
       </li>
     <?php if($_SESSION["uid"] == NULL): ?>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=login">Bejelentkezés</a>
+        <a <?php if($_GET['P'] == "login"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=login">Bejelentkezés</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=register">Regisztráció</a>
+        <a <?php if($_GET['P'] == "register"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=register">Regisztráció</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=betekinto">Betekintő</a>
+        <a <?php if($_GET['P'] == "betekinto"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=betekinto">Betekintő</a>
       </li>
       <?php else: ?>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=orarend">Órarend</a>
+        <a <?php if($_GET['P'] == "orarend"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=orarend">Órarend</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=addClass">Tárgy felvétel</a>
+        <a <?php if($_GET['P'] == "addClass"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=addClass">Tárgy felvétel</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=listClass">Felvett tárgyak</a>
+        <a <?php if($_GET['P'] == "listClass"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?> href="index.php?P=listClass">Felvett tárgyak</a>
       </li>
       
       <?php if($_SESSION["jog"] == 1): ?>
@@ -52,13 +52,13 @@
           Admin
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="index.php?P=listUser">Felhasználók szerkesztése</a>
-          <a class="dropdown-item" href="index.php?P=addNewClass">Új tárgy rögzítése</a>
+          <a <?php if($_GET['P'] == "listUser"): echo "class='dropdown-item active'"; else: echo "class='dropdown-item'"; endif; ?> href="index.php?P=listUser">Felhasználók szerkesztése</a>
+          <a <?php if($_GET['P'] == "addNewClass"): echo "class='dropdown-item active'"; else: echo "class='dropdown-item'"; endif; ?> href="index.php?P=addNewClass">Új tárgy rögzítése</a>
         </div>
       </li>
       <?php endif; ?>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?P=edit_user">Adataim</a>
+        <a <?php if($_GET['P'] == "edit_user"): echo "class='nav-link active'"; else: echo "class='nav-link'"; endif; ?>  href="index.php?P=edit_user">Adataim</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="index.php?P=logout">Kijelentkezés</a>
