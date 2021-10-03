926512397858a70229698546980a62ff4824ec0b
ricsirobi <acs.robert2000@gmail.com>
2021-10-03 17:54:19 +0200

    htdocs: timetable.php updated: clock collision fixed

--- htdocs/protected/class/timetable.php
+++ htdocs/protected/class/timetable.php
@@ -18,8 +18,6 @@
 $time = array();
 array_push($time, "", "08:00-10:00", "10:00-12:00", "12:00-14:00", "14:00-16:00", "16:00-18:00", "18:00-20:00");
 
-//            print_r($days);
-//            echo $days[0][0];
 echo "<div><table class='table table-sm table-dark text-center'>
 <th></th><th>Hétfő</th><th>Kedd</th><th>Szerda</th><th>Csütörtök</th><th>Péntek</th>";
 for ($i = 1; $i <= 6; $i++) { //órák
@@ -30,35 +28,28 @@ for ($i = 1; $i <= 6; $i++) { //órák
     for ($j = 1; $j <= 5; $j++) {
         $sql = 'select * from felhasznalok_ora,ora WHERE felhasznaloid = ' . $_SESSION["uid"] . ' and ora.id=oraid and ora.nap=' . $j . ' and ido like "' . $time[$i] . '"';
         $result = classList($sql);
-       // echo "$j.nap: $i.óra: " . count($result) . "<br>";
         if ($result === NULL || empty($result)) {
             echo "<td> </td>";
         } else {
             echo "<td>";
-            foreach ($result as $row) { //ez általában 1szer fut le viszont azért így van megoldva, hogy ha óraütközés van majd itt kell kezelni
+
+            foreach ($result as $row) { //ez általában 1szer fut le viszont azért így van megoldva, hogy ha óraütközés van majd itt lehet kezelni
                 $nap = $row['nap'];
                 $nev = $row['nev'];
                 $hely = $row['hely'];
                 $tanar = $row["tanarnev"];
                 $ido = $row['ido'];
-                //if ($nap == $j && $ido == $i)
-                    echo "<span>$nev<br>$hely<br>$tanar</span>";
-                //else {
-                //    echo "Nincs";
-                //}
+                echo "<span style='display: block'>$nev<br>$hely<br>$tanar</span>";
+                if(count($result) > 1)
+                {
+                    echo "<br>"; //az átláthatóság miatt
+                }
             }
         }
         echo "</td>";
     }
-   // echo "<br>";
     echo "</tr>";
-
 }
-
-
-
 echo "</tr>";
-
-
 
 echo "</table></div>";
